74a4cd57d5bce2f6879379dbe7a586491a63ef72
guanliang <13580594681@163.com>
2017-12-25 18:03:47 +0800

    test vue2.0

---
+++ vue1/src/component/toolcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="toolcomponent">
+    <ul>
+      <li>{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+ export default {
+  data () {
+    return {
+      item: "张三"
+    }
+  }
+ }
+</script>
+
+<style></style>
